4613ea5464b7fdaa4e7a81f319517a0b1cea8492
david <david@DESKTOP-VGPIJ5C>
2021-03-24 11:19:43 +0200

    added javadoc

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class TestThreadCheckArray {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
