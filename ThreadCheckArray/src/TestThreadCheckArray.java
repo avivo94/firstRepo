b010e842cd7ccf4a6de392fb3c26804106813da1
david <david@DESKTOP-VGPIJ5C>
2021-03-24 10:51:13 +0200

    fixed add instaed of set on array list

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -11,7 +11,7 @@ public class TestThreadCheckArray {
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++)
-				array.set(index, input.nextInt());
+				array.add( input.nextInt());
 
 			System.out.println("Enter number");
 			num = input.nextInt();
